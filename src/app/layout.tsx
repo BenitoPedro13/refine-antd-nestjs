01331e78384c410c5abe8679d0874cfe4c0131c5
Benito Pedro <benito@juicy.space>
2024-06-23 10:53:54 -0300

    removed unused resource

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,23 +55,13 @@ export default function RootLayout({
                           canDelete: true,
                         },
                       },
-                      {
-                        name: "categories",
-                        list: "/categories",
-                        create: "/categories/create",
-                        edit: "/categories/edit/:id",
-                        show: "/categories/show/:id",
-                        meta: {
-                          canDelete: true,
-                        },
-                      },
                     ]}
                     options={{
                       syncWithLocation: true,
                       warnWhenUnsavedChanges: true,
                       useNewQueryKeys: true,
                       projectId: "2ydgcl-2XAWEu-ynT0dW",
-                      title: { text: "Refine Project", icon: <AppIcon /> },
+                      title: { text: "Juicy Dash Admin", icon: <AppIcon /> },
                     }}
                   >
                     {children}
